c6eb6fac745ddbfb333b0d359bcb94d214b15cad
parth <parthshirkande96@gmail.com>
2025-06-17 17:48:03 +0530

    Clean commit with global and local .gitignore

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const  connectDB = async () =>{
+
+    await mongoose.connect('mongodb://localhost:27017/food-del').then(()=>console.log("DB Connected"));
+   
+}
+
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.  
---
+++ backend/server.js
@@ -0,0 +1,53 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import dotenv from 'dotenv';
+import userRouter from "./routes/userRoute.js"
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
+
+
+dotenv.config();
+// app config
+// create express application
+const app = express()
+const port = 4000
+
+// middleware
+app.use(express.json())
+
+//can access backend from any frontend
+app.use(cors())
+
+
+//db connections
+connectDB();
+
+//api endpoint
+app.use("/api/food",foodRouter)
+
+app.use("/api/user", userRouter) 
+
+app.use("/api/cart", cartRouter); 
+
+app.use("/api/order", orderRouter); 
+
+
+// default route
+app.get("/",(req,res)=>{
+    res.send("API WORKING")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}` )
+})
+
+//mongodb+srv://parthshirkande96:<db_password>@cluster0.1bosf.mongodb.net/?
+
+//mongodb+srv://parthshirkande96:<db_password>@cluster0.1bosf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 
+
+
+
+// mongodb+srv://parthshirkande96:<db_password>@cluster0.1bosf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
