192baeaa62eff28d9907d591c169422460818238
Parth Shirkande <parthshirkande96@gmail.com>
2025-09-13 11:29:04 +0530

    your message

--- admin-auth-backend/server.js
+++ admin-auth-backend/server.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://fooddelivery-g8cc.onrender.com', // your frontend URL
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // to serve images
 
